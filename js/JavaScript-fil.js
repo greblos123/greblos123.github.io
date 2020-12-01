b3f2f11ef082ca6676b7a3b9d42e3ae3e265404e
greblos123 <andreas@solberg.nl>
2020-12-01 09:10:36 +0100

    Initial commit

---
+++ js/JavaScript-fil.js
@@ -0,0 +1,29 @@
+var a = 5;
+var b = 10;
+var sum = a + b;
+console.log(sum)
+
+var differanse = b - a;
+console.log(differanse)
+var kvotient = b / a;
+console.log(kvotient)
+var produkt = a * b;
+console.log(produkt)
+var rest = b % 2;
+console.log(rest)
+
+if (a == b) {
+	console.log("Tallene er like!");
+}
+if (a<b) {
+	console.log("Tallet er mindre!")
+}
+else {
+		console.log("Tallet er større!")
+}
+	
+var i = 0;
+while(i < 10) {
+    console.log(i);
+    i++;
+}
