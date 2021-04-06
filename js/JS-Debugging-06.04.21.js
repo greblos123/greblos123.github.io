f918cef582d76434e170776bac38aeed830bbf3a
greblos123 <75299144+greblos123@users.noreply.github.com>
2021-04-06 15:58:28 +0200

    Add files via upload

---
+++ js/JS-Debugging-06.04.21.js
@@ -0,0 +1,21 @@
+// Debugging 06.04.2021
+
+/* Oppgave 1 */
+/* A */
+
+function syntaxError() {
+    Var tall = 5;
+    console.log(tall);
+}
+// det er stor V i Var
+
+/* B */
+
+var tall = 5;
+console.log(tall1);
+// det er ikke en variabel som heter tall1
+
+/* C */
+
+var paragraph = document.getElementsById();
+// det skal ikke være s i getElementById() og det må være noe inni ()
