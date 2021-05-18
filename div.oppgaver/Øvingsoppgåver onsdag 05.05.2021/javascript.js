98b2863e9a27a072027803323507144707968e15
greblos123 <75299144+greblos123@users.noreply.github.com>
2021-05-18 13:18:35 +0200

    yes
    yes

---
+++ div.oppgaver/Øvingsoppgåver onsdag 05.05.2021/javascript.js
@@ -0,0 +1,14 @@
+// Oppgave #1 //
+antallFrukt = 3;
+antallGronnsaker = 2;
+
+antallVarer = antallFrukt + antallGronnsaker;
+
+console.log(antallVarer);
+// Oppgave #2 //
+prisUtenMoms = prompt("Hva er prisen uten moms?");
+
+prisMedMoms = Number(prisUtenMoms) * 1.25;
+
+alert("Pris med moms: " + prisMedMoms.toFixed(2));
+// Oppgave #3 //
